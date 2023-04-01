581d2e07021d9f4eec0a999792230065f8d11b12
Luka <luka.levac@gmail.com>
2023-04-01 18:34:49 +0100

    home update

--- client/src/components/PanelHome.js
+++ client/src/components/PanelHome.js
@@ -1,4 +1,4 @@
-import {Col, Container, ListGroup, Row, Button} from "react-bootstrap";
+import {Col, Container, Card, Row, Button} from "react-bootstrap";
 
 import { AppContext } from "../Context/context";
 
@@ -14,22 +14,45 @@ const PanelHome = () => {
 
 
     return (
-        <Container className="d-flex flex-column" style={{backgroundColor: "#F2E0C2", height: "620px"}}>
+        <Container className="d-flex flex-column" style={{backgroundColor: "#F2E0C2", height: "800px"}}>
+            <Row>
+                 <div >
+                    <img src="A1_logo.png" alt="A1 logo" className="logo" style={{width: "120px", height: "120px", alignSelf: "center"}} />
+                </div>
+            </Row>
+            <Row>
+            {/* Tel A1: 040 758 948: Jernej */}
+                <div style={{justifyContent: "center", alignItems: "center", boxShadow: "var(--light-shadow)"}}>
+                    <Card style={{marginTop: "3rem", width: "100%", justifyContent: "center", alignItems: "center", alignSelf: "center", backgroundColor: "#C0322A", color: "white", borderRadius: "8px"}}>
+                        <Card.Body>
+                            <Card.Title style={{fontFamily: "DOS",  fontSize: "2rem"}}>Dobrodosli v A1 spletni aplikaciji</Card.Title>
+                            <Card.Text style={{fontFamily: "DOS",  fontSize: "2rem"}}>
+                                {/* dsfdsfdfd. */}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                
+            </Row>
+            <Row>
+                <Col>             
+                    <div style={{float: 'left'}}>
+                        <Button variant="outline-dark"  href="/profile" style={{fontFamily: "DOS",  fontSize: "2rem", marginTop: "3rem", borderWidth: "3px", marginLeft: "0"}}>Profil</Button>
+                    </div>
+                </Col>
+            </Row>
             <Row>
                 <Col>
-                    <div >
-                        <img src="A1_logo.png" alt="A1 logo" className="logo" style={{width: "120px", height: "120px", alignSelf: "center"}} />
-                    </div>
-                    
-                    <div style={{float: 'left'}}>
-                        <Button href="/profile" style={{fontFamily: "DOS",  fontSize: "2rem", backgroundColor: "black", marginTop: "4rem", borderWidth: "0", marginLeft: "0"}}>Profil</Button>
-                    </div>
-                    <div style={{float: 'right'}}>
-                        <Button href="/profile" style={{fontFamily: "DOS",  fontSize: "2rem", backgroundColor: "black", marginTop: "8rem", borderWidth: "0", marginLeft: "0"}}>Profil</Button>
-                    </div>
-                    <div style={{float: 'left'}}>
-                        <Button href="/profile" style={{fontFamily: "DOS",  fontSize: "2rem", backgroundColor: "black", marginTop: "4rem", borderWidth: "0", marginLeft: "0"}}>Profil</Button>
-                    </div>
+                <div style={{float: 'right'}}>
+                    <Button variant="outline-dark" href="/store" style={{fontFamily: "DOS",  fontSize: "2rem", marginTop: "3rem", borderWidth: "3px", marginLeft: "0"}}>Trgovina</Button>
+                </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <div style={{float: 'left'}}>
+                    <Button variant="outline-dark" href="/forum" style={{fontFamily: "DOS",  fontSize: "2rem", marginTop: "3rem", borderWidth: "3px", marginLeft: "0"}}>Forum</Button>
+                </div>
                 </Col>
             </Row>
         </Container>
